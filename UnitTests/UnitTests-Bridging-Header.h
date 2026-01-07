5c05f8c5e289543032eb7928b846e10cc3b4bd5f
Gigi <42325924+g-cqd@users.noreply.github.com>
2026-01-07 16:25:46 +0100

    Fix Swift bridging header imports

--- UnitTests/UnitTests-Bridging-Header.h
+++ UnitTests/UnitTests-Bridging-Header.h
@@ -1,6 +1,8 @@
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
 //  Copyright (C) 1997-2019 Steve Nygard.
 
+#import <Foundation/Foundation.h>
+
 #import "CDFile.h"
 #import "CDFatArch.h"
 #import "CDFatFile.h"
