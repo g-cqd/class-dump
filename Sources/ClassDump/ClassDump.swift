a95e7c52c3c869e74e8a2baa44ea5f34707d15d4
Gigi <42325924+g-cqd@users.noreply.github.com>
2026-01-08 12:26:35 +0100

    Formatting

--- Sources/ClassDump/ClassDump.swift
+++ Sources/ClassDump/ClassDump.swift
@@ -2,8 +2,8 @@
 //
 // This module re-exports all class-dump components for unified documentation.
 
-@_exported import ClassDumpCore
 @_exported import ClassDumpCLI
+@_exported import ClassDumpCore
 @_exported import DeprotectCLI
 @_exported import FormatTypeCLI
 @_exported import RegressionTestCLI
